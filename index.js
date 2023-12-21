eb5c20ba1944c0cb6bcb8ae131064b5d39baa92f
Stacey Sanchez <staceysanchez27@gmail.com>
2023-12-20 21:50:17 -0500

    questions 3 and 4

--- index.js
+++ index.js
@@ -67,3 +67,94 @@ tree4.right = new TreeNode(2);
 console.log("Example 2: isSameTree(tree3, tree4) =", isSameTree(tree3, tree4)); 
 
 //Question 3 
+class BinaryTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function countNodes(binaryTreeRoot) {
+    if (binaryTreeRoot === null) {
+        return 0;
+    }
+
+    let leftHeight = 0;
+    let rightHeight = 0;
+    let left = binaryTreeRoot.left;
+    let right = binaryTreeRoot.right;
+
+    while (left !== null) {
+        left = left.left;
+        leftHeight++;
+    }
+
+    while (right !== null) {
+        right = right.right;
+        rightHeight++;
+    }
+
+    if (leftHeight === rightHeight) {
+        return Math.pow(2, leftHeight + 1) - 1;
+    }
+
+    return 1 + countNodes(binaryTreeRoot.left) + countNodes(binaryTreeRoot.right);
+}
+
+//examples
+const rootNode = new BinaryTreeNode(10);
+rootNode.left = new BinaryTreeNode(11);
+rootNode.right = new BinaryTreeNode(12);
+rootNode.left.left = new BinaryTreeNode(13);
+rootNode.left.right = new BinaryTreeNode(14);
+rootNode.right.left = new BinaryTreeNode(15);
+rootNode.right.right = new BinaryTreeNode(16);
+
+const nodeCount = countNodes(rootNode);
+console.log("Number of nodes in the binary tree:", nodeCount); 
+
+
+//Question 4 
+class TreeNode2 {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function findBottomRightValue(root) {
+    let result = null;
+    const queue = [];
+
+    if (root) {
+        queue.push(root);
+
+        while (queue.length > 0) {
+            const node = queue.shift();
+            result = node.value;  
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+    }
+
+    return result;
+}
+
+// Example usage
+const treeRoot = new TreeNode2(2);
+treeRoot.left = new TreeNode(21);
+treeRoot.right = new TreeNode2(3);
+treeRoot.left.left = new TreeNode2(4);
+treeRoot.left.right = new TreeNode2(5);
+treeRoot.right.right = new TreeNode2(6);
+
+const bottomRightValue = findBottomRightValue(treeRoot);
+console.log("Bottom rightmost value:", bottomRightValue); 
